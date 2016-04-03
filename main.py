2a48cb33212eb8b1a994f6e63acb5656ca78a28c
Pistolnik <darthokosy@gmail.com>
2016-04-03 22:30:37 +0200

    cleaning after fuck up

--- main.py
+++ main.py
@@ -2,17 +2,10 @@ from firstClass import Employee
 
 def doStuff(stuff):
     print stuff, stuff
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> 4786e7b017f03c5977a312739ea2169d86938cb7
 if __name__ == '__main__':
     prvni = Employee("Pavel",10000)
     print "Num of employees: ", Employee.empCount
     prvni.displayEmployee()
-<<<<<<< HEAD
     prvni.displayEmployee()
-=======
->>>>>>> 4786e7b017f03c5977a312739ea2169d86938cb7
     doStuff("tada")
