9fbbed68f63ff2a512d0c1392fd03ce9e9b6260d
Pistolnik <darthokosy@gmail.com>
2016-04-03 19:53:25 +0200

    main doStuff added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from firstClass import Employee
 
+def doStuff(stuff):
+    print stuff, stuff
+
 if __name__ == '__main__':
     prvni = Employee("Pavel",10000)
     print "Num of employees: ", Employee.empCount
     prvni.displayEmployee()
+    doStuff("tada")
