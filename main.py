7636129799a9c36ebac50eed5d1d223b62b6d36b
Pistolnik <darthokosy@gmail.com>
2016-04-04 06:30:57 +0200

    from edit main editing main j4f

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from firstClass import Employee
 
+def doStuffing():
+    print "Say HW"
+
 if __name__ == '__main__':
     prvni = Employee("Pavel",10000)
     print "Num of employees: ", Employee.empCount
